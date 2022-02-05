76f560f054cf6b39a2a8cdf368c6f15703c32420
Takeshi Kiyota <t.kiyota@gxp.co.jp>
2022-02-05 14:59:41 +0900

    add test

---
+++ README.md
@@ -0,0 +1,8 @@
+# fizzBuzz
+
+- interface で fizzBuzz のルールを変えるコードの実践
+  - interface 単位でテストが書かけることも確認
+
+## requirement
+
+- jdk 11
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,13 +1,11 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
-    void name() {
+    void exec() {
         Rule normalRule = new NormalRule();
         FizzBuzz fizzBuzz = new FizzBuzz(normalRule);
         fizzBuzz.doFizzBuzz(1, 100);
@@ -17,5 +15,33 @@ class MainTest {
         FizzBuzz fizzBuzz2 = new FizzBuzz(leapYearRule);
         fizzBuzz2.doFizzBuzz(1, 20);
         System.out.println(fizzBuzz2);
+    }
+
+    @Test
+    void normalRule() {
+        NormalRule normalRule = new NormalRule();
+        String actual = normalRule.checkFizzBuzz(3);
+        String actual2 = normalRule.checkFizzBuzz(5);
+        String actual3 = normalRule.checkFizzBuzz(15);
+        String actual4 = normalRule.checkFizzBuzz(2);
+
+        assertEquals(actual, "Fizz");
+        assertEquals(actual2, "Buzz");
+        assertEquals(actual3, "FizzBuzz");
+        assertEquals(actual4, "");
+    }
+
+    @Test
+    void leapYearRule() {
+        LeapYearRule leapYearRule = new LeapYearRule();
+        String actual = leapYearRule.checkFizzBuzz(4);
+        String actual2 = leapYearRule.checkFizzBuzz(100);
+        String actual3 = leapYearRule.checkFizzBuzz(200);
+        String actual4 = leapYearRule.checkFizzBuzz(400);
+
+        assertEquals(actual, "うるう年です");
+        assertEquals(actual2, "");
+        assertEquals(actual3, "");
+        assertEquals(actual4, "うるう年です");
     }
 }
